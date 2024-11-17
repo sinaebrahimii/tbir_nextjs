e96c511fd5f048256ae60f5f85b4d5b06e08c5de
sina ebrahimi <sinaebrahimi31@gmail.com>
2024-11-18 00:33:50 +0330

    url fixed to https

--- app/photos/[id]/page.tsx
+++ app/photos/[id]/page.tsx
@@ -20,7 +20,7 @@ import { Input } from "@/components/ui/input";
 const getPhoto = async (id: string): Promise<ImageResponse> => {
   // Perform the mutation logic, e.g., make an API request to update the user
 
-  const response = await fetch(`http://fastapi.darkube.app/photos/${id}`);
+  const response = await fetch(`https://fastapi.darkube.app/photos/${id}`);
 
   if (!response.ok) {
     throw new Error("Failed to update user");
